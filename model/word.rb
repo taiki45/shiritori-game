7575ddbce7918aa9ca8d6293448d12fa5359d628
Taiki ONO <taiks.4559@gmail.com>
2014-01-19 01:32:16 +0900

    init

---
+++ model/word.rb
@@ -0,0 +1,13 @@
+class Word
+  attr_reader :origin, :normal_form
+
+  def initialize(origin, normal_form)
+    @origin = origin
+    @normal_form = normal_form
+  end
+
+  def ==(other)
+    return false unless is_a? other.class
+    @normal_form == other.normal_form
+  end
+end
